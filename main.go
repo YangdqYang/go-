f934c71148d68cc527f2869e71ffbd7f0ac7b5ea
yhy2015 <yang2868648@163.com>
2018-08-08 10:12:04 +0800

    随便写写

--- main.go
+++ main.go
@@ -9,21 +9,54 @@ import (
 type Person struct {
 	Name string `json:"name"`
 	Age int `json:"age"`
+	Dogs []Dog `json:"dogs"`
+}
+
+type Dog struct {
+	Name string `json:"name"`
+	Cag string `json:"cag"`
 }
 
 func main()  {
-	r := gin.Default()
-	r.GET("/string", func(c *gin.Context) {
-		c.String(http.StatusOK, "it works")
-	})
+	Hello()
 
-	r.GET("/json", func(c *gin.Context) {
-		data := Person{Name: "yang", Age: 12}
+	r := gin.Default()
+	r.GET("/user/:name", func(c *gin.Context) {
+		type user struct {
+			Name string `json:"name"`
+			Age string `json:"age"`
+			Height string `json:"height"`
+		}
+		name := c.Param("name")
+		age := c.Query("age")
+		height := c.Query("height")
+
+		data := user{Name: name, Age: age, Height: height}
 		c.JSON(http.StatusOK, gin.H{
 			"errorCode": 0,
 			"data" : data,
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+
+
+	r.GET("/string", func(c *gin.Context) {
+		c.String(http.StatusOK, "it works")
+	})
+
+	r.GET("/json", func(c *gin.Context) {
+		var s []Dog
+		s = append(s, Dog{Name: "小贝贝", Cag: "泰迪"})
+		s = append(s, Dog{Name: "小嘿嘿", Cag: "泰迪"})
+		s = append(s, Dog{Name: "白丹", Cag: "牧羊犬"})
+
+		data := Person{Name: "yang", Age: 12, Dogs: s}
+
+		c.JSON(http.StatusOK, gin.H{
+			"errorCode": 0,
+			"data" : data,
+		})
+	})
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 	fmt.Println("hello world")
 }
